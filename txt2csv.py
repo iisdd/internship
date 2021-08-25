1a4b724b72a7a5f6b9faf9479a37d6af598694e0
dd <59326234+iisdd@users.noreply.github.com>
2021-08-25 10:52:15 +0800

    Update txt2csv.py

--- txt2csv.py
+++ txt2csv.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 def txt2csv(txt_name, output_file_name, index=['x','y','z','delay','plr'], delay=[50, 100], plr=[5, 20, 35, 50]):
     data = pd.read_csv(txt_name, sep='	', header=None, names=index)
-    data['delay']=data['delay'].replace(delay[0],0)
-    data['delay']=data['delay'].replace(delay[1],1)
+    # 替换延迟量
+    for d in range(len(delay)):
+        data['delay'] = data['delay'].replace(delay[d], d)
+    # 替换偏振角
+    for p in range(len(plr)):
+        data['plr'] = data['plr'].replace(plr[p], p)
 
-    data['plr']=data['plr'].replace(plr[0],0)
-    data['plr']=data['plr'].replace(plr[1],1)
-    data['plr']=data['plr'].replace(plr[2],2)
-    data['plr']=data['plr'].replace(plr[3],3)
 
-    data['decode'] = data['plr'] + data['delay'] * 4      # 用来画聚类图的总标签
+    data['decode'] = data['plr'] + data['delay'] * len(plr)      # 用来画聚类图的总标签
+    print(data.info())
     data.to_csv(output_file_name, index=False)
+    print(data.head())
